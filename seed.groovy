21ecb7e1dfd3a43bfffd2409a4a8ab23561ad987
My <karumanchi.swapna@gmail.com>
2022-03-16 19:23:15 +0530

    new jenkin file

---
+++ seed.groovy
@@ -0,0 +1,17 @@
+freeStyleJob('example') {
+          scm {
+                      git {
+           remote {
+               name("origin")
+               url("https://github.com/karumanchi06/jenkins.git")
+
+           }
+        }
+    }
+
+    steps {
+        shell(ls - ltr)
+    }
+}
+}
+
